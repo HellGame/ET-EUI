39aec3db553b91ffa063b0f5c83b82325090f69b
LiBing <libing@ofunagames.com>
2022-03-28 11:14:56 +0800

    23 - 初始化区服信息，配置表配置好然后存入数据库。

--- Server/Hotfix/Demo/ServerInfo/ServerInfoManagerComponentSystem.cs
+++ Server/Hotfix/Demo/ServerInfo/ServerInfoManagerComponentSystem.cs
@@ -38,6 +38,21 @@ namespace ET
             if (serverInfoList == null || serverInfoList.Count <= 0)
             {
                 Log.Error("ServerInfo count is 0");
+
+                self.ServerInfoList.Clear();
+                
+                // 使用配置表来初始化区服信息，实战中是需要用 GM 指令来配置。
+                var serverInfoConfigs = ServerInfoConfigCategory.Instance.GetAll();
+                foreach (var info in serverInfoConfigs.Values)
+                {
+                    ServerInfo newServerInfo = self.AddChildWithId<ServerInfo>(info.Id);
+                    newServerInfo.ServerName = info.ServerName;
+                    newServerInfo.Status = (int)ServerStatus.Normal;
+                    self.ServerInfoList.Add(newServerInfo);
+
+                    await DBManagerComponent.Instance.GetZoneDB(self.DomainZone()).Save(newServerInfo);
+                }
+                
                 return;
             }
             
